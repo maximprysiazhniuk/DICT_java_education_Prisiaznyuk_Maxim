4554df4885ba82ce75d3b3d32d59f786faa4e3b4
maximprysiazhniuk <m.i.prysiazhiuk@student.khai.edu>
2022-02-05 20:41:18 +0200

    ChatBot.java 1 stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,12 @@
+package ChatBot;
+
+import java.util.Scanner;
+
+public class ChatBot {
+
+    public static void main(String[] args){
+        System.out.println("Hello! My name is " + "Chatty" + ".");
+        System.out.println("I was created in " + "2022" + ".");
+
+    }
+}
