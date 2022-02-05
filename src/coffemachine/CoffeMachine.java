0b3b9c75d04acd725555aeddc487fbd6b817dfb7
maximprysiazhniuk <m.i.prysiazhiuk@student.khai.edu>
2022-02-06 01:23:13 +0200

    Coffemachine 1 stage

---
+++ src/coffemachine/CoffeMachine.java
@@ -0,0 +1,9 @@
+package CoffeMachine;
+
+public class CoffeMachine {
+    public static void main(String[] args) {
+        System.out.println("Starting to make a coffee\nGrinding coffee beans\nBoiling water\n" +
+                "Mixing boiled water with crushed coffee beans\nPouring coffee into the cup\n" +
+                "Pouring some milk into the cup\nCoffee is ready!");
+    }
+}
