2f37f873cf094d800fba6d1e184f87df787cbe30
maximprysiazhniuk <m.i.prysiazhiuk@student.khai.edu>
2022-02-05 21:04:42 +0200

    ChatBot.java 3 stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,12 +1,11 @@
 package ChatBot;
-
 import java.util.Scanner;
-
 public class ChatBot {
     final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         greet();
         remindName();
+        guessAge();
     }
 
     static void greet() {
@@ -18,5 +17,15 @@ public class ChatBot {
     {
         String name = scanner.nextLine();
         System.out.println("What a great name you have, " + name + "!");
+    }
+    static void guessAge()
+    {
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
