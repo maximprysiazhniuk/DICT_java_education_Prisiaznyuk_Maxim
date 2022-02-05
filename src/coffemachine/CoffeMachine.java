eb6c3542b5d93bc4500df0207e660868a50e3254
maximprysiazhniuk <m.i.prysiazhiuk@student.khai.edu>
2022-02-06 01:31:13 +0200

    Coffemachine 3 stage

--- src/coffemachine/CoffeMachine.java
+++ src/coffemachine/CoffeMachine.java
@@ -3,10 +3,47 @@ import java.util.Scanner;
 
 public class CoffeMachine {
     public static void main(String[] args) {
-        System.out.println("Write how many cups of coffee you will need:");
+        System.out.println("Write how many ml of water the coffee machine has:");
         Scanner userInput = new Scanner(System.in);
-        int cofeeCups = Integer.parseInt(userInput.nextLine());
-        System.out.printf("For %d cups of coffee you will need:\n%d ml of water\n%d ml of milk\n%d g of coffee beans",
-                cofeeCups, cofeeCups * 200, cofeeCups * 50, cofeeCups * 15);
+        int water = Integer.parseInt(userInput.nextLine());
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = Integer.parseInt(userInput.nextLine());
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beans = Integer.parseInt(userInput.nextLine());
+        System.out.println("Write how many cups of coffee you need");
+        int coffeeNeed = Integer.parseInt(userInput.nextLine());
+        int coffeeCups = coffeeCount(water, milk, beans);
+
+        if (coffeeCups == coffeeNeed) {
+            System.out.println("Yes, i can do this");
+        }
+        else if (coffeeCups > coffeeNeed){
+            System.out.printf("Yes, i can do this (and even %d more than that)", coffeeCups - coffeeNeed);
+        }
+        else {
+            System.out.printf("No, i can do only %d cups of coffee", coffeeCups);
+        }
+    }
+
+    public static int coffeeCount (int water, int milk, int beans) {
+        int coffeeCups = 0;
+
+        while (true) {
+            if (water - 200 < 0) {
+                break;
+            }
+            water = water - 200;
+            if (milk - 50 < 0) {
+                break;
+            }
+            milk = milk - 50;
+            if (beans -15 < 0) {
+                break;
+            }
+            beans = beans - 15;
+            coffeeCups += 1;
+        }
+
+        return coffeeCups;
     }
 }
