717ebfac48ff3ad1cb589220d145439902d92d44
maximprysiazhniuk <m.i.prysiazhiuk@student.khai.edu>
2022-02-05 21:26:23 +0200

    Hangman 2 stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,7 +1,19 @@
 package Hangman;
+import java.util.Scanner;
 
 public class Hangman {
-    public static void main(String[] args) {
-        System.out.println("HANGMAN\nThe game will be available soon");
+    public static void main(String[] args) {;
+        String word = "java";
+        System.out.println("HANGMAN");
+        System.out.println("Guess the word:");
+        Scanner user_input = new Scanner(System.in);
+        String answer = user_input.nextLine();
+        if (answer.equals(word)) {
+            System.out.println("You survived!");
+        }
+        else {
+            System.out.println("You lost");
+        }
+
     }
 }
